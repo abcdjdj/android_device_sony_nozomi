8d6cb45759aa0949eefefc63fc87eba82d3f3c51
abcdjdj <abcdjdj@gmail.com>
2014-04-09 16:58:13 +0530

    Patches for elohim rom

--- elohim_nozomi.mk
+++ elohim_nozomi.mk
@@ -17,19 +17,19 @@
 $(call inherit-product, device/sony/nozomi/full_nozomi.mk)
 
 # Inherit CM common GSM stuff.
-$(call inherit-product, vendor/carbon/config/common_gsm.mk)
+$(call inherit-product, vendor/elohim/config/gsm.mk)
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/carbon/config/common_phone.mk)
+$(call inherit-product, vendor/elohim/config/common.mk)
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/carbon/config/common_phone.mk)
+$(call inherit-product, vendor/elohim/config/common.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i_1257-5499 BUILD_FINGERPRINT=SEMC/LT26i_1257-5499/LT26i:4.0.4/6.1.A.2.50/zfd_zw:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.0.4 6.1.A.2.50 zfd_zw test-keys"
 
-PRODUCT_NAME := carbon_nozomi
+PRODUCT_NAME := elohim_nozomi
 PRODUCT_DEVICE := nozomi
--- full_nozomi.mk
+++ full_nozomi.mk
@@ -25,7 +25,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/sony/nozomi/nozomi.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_nozomi
+PRODUCT_NAME := elohim_nozomi
 PRODUCT_DEVICE := nozomi
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
